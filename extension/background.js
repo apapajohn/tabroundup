8d08ba618c0ec3e006a8724f49be66474981baa2
April.Papajohn <april.papajohn@wolterskluwer.com>
2025-09-01 15:00:24 -0400

    Was grouping too much

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,5 @@
 const collator = new Intl.Collator();
-const minTabs = 1; //minimum number of tabs to group
+const minTabs = 10; //minimum number of tabs to start grouping
 const tabQueryOpts = {
     windowType: "normal"
 };
@@ -53,43 +53,56 @@ async function sortAllTabsAndMergeWindows(tabs, options) {
         );
     }
 }
-async function groupTabs(tabs, options) {
-    //group tabs only if the window is running out of room
-    if (tabs.length < minTabs) return;
+async function groupTabs(allTabs, options) {
 
-    const setDupeIds = findDupes(tabs);
-    //if the option to do so was selected, remove all the dupes we just found (otherwise, they'll be placed in a special "dupes" tab group):
-    if (options.deleteDuplicateTabs) {
-        tabs.forEach((tab) => {
-            if (setDupeIds.has(tab.id)) {
-                chrome.tabs.remove(tab.id);
-            }
-        });    
-        setDupeIds.clear();
-    }
-
-    const groups = tabs.reduce(function (m, tab) {
-        let domain = setDupeIds.has(tab.id)  ? "dupes" : tab.url.replace(/http.+\/\//, '').replace(/\/.*/, '');
-        if (!m.has(domain)) {
-            m.set(domain, []);
+    //group tabs by window:
+    const tabMap = allTabs.reduce(function (m, tab) {
+        if (!m.has(tab.windowId)) {
+            m.set(tab.windowId, []);
         }
-        m.get(domain).push(tab);
+        m.get(tab.windowId).push(tab);
         return m;
     }, new Map());
 
-    for (let e of groups.keys()) {
-        let tabGroup = groups.get(e);
-        if (tabGroup.length > 1 || e === "dupes") {
-            let groupTitle = e.replace(/www./, '').replace(/\..+$/, '');
-            let options = {tabIds: tabGroup.map(t => t.id)};
-            let tabGroups = await chrome.tabGroups.query({title: groupTitle});
-            if (tabGroups.length > 0) {
-                options.groupId = tabGroups[0].id;
+    //group tabs only if the window is running out of room
+    tabMap.forEach(async (tabs, windowId) => {
+        if (tabs.length < minTabs) return;
+
+        const setDupeIds = findDupes(tabs);
+        //if the option to do so was selected, remove all the dupes we just found (otherwise, they'll be placed in a special "dupes" tab group):
+        if (options.deleteDuplicateTabs) {
+            tabs.forEach((tab) => {
+                if (setDupeIds.has(tab.id)) {
+                    chrome.tabs.remove(tab.id);
+                }
+            });
+            setDupeIds.clear();
+        }
+
+        const groups = tabs.reduce(function (m, tab) {
+            let domain = setDupeIds.has(tab.id) ? "dupes" : tab.url.replace(/http.+\/\//, '').replace(/\/.*/, '');
+            if (!m.has(domain)) {
+                m.set(domain, []);
             }
-            chrome.tabs.group(options, (groupId) => chrome.tabGroups.update(groupId, {
-                title: groupTitle,
-                collapsed: tabGroup.length > 2
-            }));
+            m.get(domain).push(tab);
+            return m;
+        }, new Map());
+
+        for (let e of groups.keys()) {
+            let tabGroup = groups.get(e);
+            if (tabGroup.length > 1 || e === "dupes") {
+                let groupTitle = e.replace(/www./, '').replace(/\..+$/, '');
+                let options = { tabIds: tabGroup.map(t => t.id) };
+                let tabGroups = await chrome.tabGroups.query({ title: groupTitle });
+                if (tabGroups.length > 0) {
+                    options.groupId = tabGroups[0].id;
+                }
+                chrome.tabs.group(options, (groupId) => chrome.tabGroups.update(groupId, {
+                    title: groupTitle,
+                    collapsed: tabGroup.length > 2
+                }));
+            }
         }
     }
+);
 }
